c69ef13fbdfdd8bb15db79fe4f29a4ffda534572
n-ni2 <80830182+n-ni2@users.noreply.github.com>
2021-04-14 17:57:12 +0200

    python file for creating a switch widget
    QtDesigner has no switches.
    push button is getting modified to look like a switch.

---
+++ SwitchCustomWidget.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 15:56:39 2021
+
+@author: Nithu
+"""
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QRect
+import sys
+
+class ChangeSet(QtWidgets.QPushButton):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setCheckable(True)
+        self.setMinimumWidth(66)
+        self.setMinimumHeight(22)
+
+    def paintEvent(self, event):
+        label = "SET2" if self.isChecked() else "SET1"
+        bg_color = Qt.white
+
+        radius = 10
+        width = 50
+        center = self.rect().center()
+
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.translate(center)
+        painter.setBrush(QtGui.QColor(0,0,0))
+
+        pen = QtGui.QPen(Qt.black)
+        pen.setWidth(2)
+        painter.setPen(pen)
+
+        painter.drawRoundedRect(QRect(-width, -radius, 2*width, 2*radius), radius, radius)
+        painter.setBrush(QtGui.QBrush(bg_color))
+        sw_rect = QRect(-radius, -radius, width + radius, 2*radius + 2)
+        if not self.isChecked():
+            sw_rect.moveLeft(-width)
+        painter.drawRoundedRect(sw_rect, radius, radius)
+        painter.drawText(sw_rect, Qt.AlignCenter, label)
