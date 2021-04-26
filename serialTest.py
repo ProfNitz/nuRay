3c8f907ae173add2e808fe66ceb0f142ae6ce772
n-ni2 <80830182+n-ni2@users.noreply.github.com>
2021-04-27 00:18:13 +0200

    updating inputs of method write(...)
    pset and pidx are being added to the input of write instead of only cmd

--- serialTest.py
+++ serialTest.py
@@ -12,20 +12,20 @@ if __name__=='__main__':
     s = nuRSerial()
     s.port='COM8'
     s.connect()
-    s.write(0,0,'uint8')#a first package, which will be ignored because no preceding 0xff
+    s.write(1,0,0,'uint8')#a first package, which will be ignored because no preceding 0xff
     #a simple example
-    s.write(12,-125,'float32')
+    s.write(0,1,-125,'float32')
     # a tricky example, because payload contains stopflag 0xff
-    s.write(12,3.57331108403e-43,'float32')
+    s.write(1,2,3.57331108403e-43,'float32')
     # another tricky example, because payload contains startflag 0xffe
-    s.write(12,9.11180313443e-41,'float32')
+    s.write(1,2,9.11180313443e-41,'float32')
     
     for k in range(30):
-        s.write(12,k,'float32')
+        s.write(1,0,k,'float32')
     
-    s.write(10,12345,'int16')
-    s.write(10,-12345,'int16')
-    s.write(14,5,'uint8')
+    s.write(1,1,12345,'int16')
+    s.write(0,1,-12345,'int16')
+    s.write(1,1,5,'uint8')
     
     s.close()
     
