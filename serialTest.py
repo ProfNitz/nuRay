ff9be48cddee86f46a6e61fa3a41f100fa1f37bf
n-ni2 <80830182+n-ni2@users.noreply.github.com>
2021-04-27 12:28:50 +0200

    modifying testinput pidx of method write
    higher range of input pidx
    instead of only zero and one more options because now we have the choice to generate more parameters automatically

--- serialTest.py
+++ serialTest.py
@@ -12,20 +12,20 @@ if __name__=='__main__':
     s = nuRSerial()
     s.port='COM8'
     s.connect()
-    s.write(1,0,0,'uint8')#a first package, which will be ignored because no preceding 0xff
+    s.write(1,29,0,'uint8')#a first package, which will be ignored because no preceding 0xff
     #a simple example
-    s.write(0,1,-125,'float32')
+    s.write(0,25,-125,'float32')
     # a tricky example, because payload contains stopflag 0xff
-    s.write(1,2,3.57331108403e-43,'float32')
+    s.write(1,20,3.57331108403e-43,'float32')
     # another tricky example, because payload contains startflag 0xffe
-    s.write(1,2,9.11180313443e-41,'float32')
+    s.write(1,15,9.11180313443e-41,'float32')
     
     for k in range(30):
         s.write(1,0,k,'float32')
     
-    s.write(1,1,12345,'int16')
-    s.write(0,1,-12345,'int16')
-    s.write(1,1,5,'uint8')
+    s.write(1,10,12345,'int16')
+    s.write(0,8,-12345,'int16')
+    s.write(1,17,5,'uint8')
     
     s.close()
     
