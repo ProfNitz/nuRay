966a2aa295ee0630ac1b35ae164fc4170d60be72
n-ni2 <80830182+n-ni2@users.noreply.github.com>
2021-05-13 21:34:18 +0200

    delay added with timer
    a delay is needed to make it work properly

--- Comm/Arduino Serial Communication/arduinotest.py
+++ Comm/Arduino Serial Communication/arduinotest.py
@@ -6,14 +6,17 @@ Created on Thu May 13 17:27:50 2021
 """
 
 import serial
+import time
 
-arduinoNano = serial.Serial('COM7',115200)
+arduinoNano = serial.Serial('COM10',115200)
 
 def led_On():
+    time.sleep(3)
     arduinoNano.write(b'1')
     print("1 has been sent to arduino")
 
 def led_Off():
+    time.sleep(3)
     arduinoNano.write(b'0')
     print("0 has been sent to arduino")
     
