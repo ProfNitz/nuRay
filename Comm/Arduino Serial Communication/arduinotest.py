5e811f608af5a85cce5c839852524b807bd98f86
n-ni2 <80830182+n-ni2@users.noreply.github.com>
2021-05-14 12:36:15 +0200

    created an UI to turn on and off LED
    delay is not needed anymore
    buttons created with QtDesigner
    buttons connected to methods to control LED

--- Comm/Arduino Serial Communication/arduinotest.py
+++ Comm/Arduino Serial Communication/arduinotest.py
@@ -7,20 +7,33 @@ Created on Thu May 13 17:27:50 2021
 
 import serial
 import time
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5 import uic
+import sys
+   
 
-arduinoNano = serial.Serial('COM10',115200)
+class UI(QWidget):
+    arduinoNano = serial.Serial('COM10',115200)
+    def __init__(self):
+        QWidget.__init__(self)
+        uic.loadUi("ArduinoTest.ui",self)
+        self.setWindowTitle("ArduinoTest")
+        self.pushButton_2.clicked.connect(self.led_On)
+        self.pushButton.clicked.connect(self.led_Off)
+        self.show()
 
-def led_On():
-    time.sleep(3)
-    arduinoNano.write(b'1')
-    print("1 has been sent to arduino")
+    def led_On(self):
+        self.arduinoNano.write(b'1')
+        print("1 has been sent to arduino")
 
-def led_Off():
-    time.sleep(3)
-    arduinoNano.write(b'0')
-    print("0 has been sent to arduino")
-    
-led_On()
+    def led_Off(self):
+        self.arduinoNano.write(b'0')
+        print("0 has been sent to arduino")
+
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
+
 
 
     
